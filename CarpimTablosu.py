ad2d9f0c3bd7c7a4b5209eecb07cad274bb79cc6
Ferda <ferda.o@outlook.com>
2020-04-23 09:47:40 +0300

    second commit

--- CarpimTablosu.py
+++ CarpimTablosu.py
@@ -1,6 +1,10 @@
 i=1
 j=1
 
-while i < 11:
-    while j < 11:
+while i <= 10:
+    while j <= 10:
         print("{} * {} = {}".format(i, j, i*j))
+        j += 1
+    j = 1
+    i += 1
+    print('--------------------------')
